7b2ce9789a6227f014ee80022111a990a8227a61
xNad16 <52595003+xNad16@users.noreply.github.com>
2019-07-06 16:56:18 +0800

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,26 @@
+# Python Code Style
+reformat:
+	black -l 99 `git ls-files "*.py"`
+stylecheck:
+	black --check -l 99 `git ls-files "*.py"`
+
+# Translations
+gettext:
+	redgettext --command-docstrings --verbose --recursive redbot --exclude-files "redbot/pytest/**/*"
+upload_translations:
+	$(MAKE) gettext
+	crowdin upload sources
+download_translations:
+	crowdin download
+
+# Dependencies
+bumpdeps:
+	python tools/bumpdeps.py
+
+# Development environment
+newenv:
+	python3.7 -m venv --clear .venv
+	.venv/bin/pip install -U pip setuptools
+	$(MAKE) syncenv
+syncenv:
+	.venv/bin/pip install -Ur ./tools/dev-requirements.txt
---
+++ make.bat
@@ -0,0 +1,42 @@
+@echo off
+
+if [%1] == [] goto help
+
+REM This allows us to expand variables at execution
+setlocal ENABLEDELAYEDEXPANSION
+
+REM This will set PYFILES as a list of tracked .py files
+set PYFILES=
+for /F "tokens=* USEBACKQ" %%A in (`git ls-files "*.py"`) do (
+    set PYFILES=!PYFILES! %%A
+)
+
+goto %1
+
+:reformat
+black -l 99 !PYFILES!
+exit /B %ERRORLEVEL%
+
+:stylecheck
+black -l 99 --check !PYFILES!
+exit /B %ERRORLEVEL%
+
+:newenv
+py -3.7 -m venv --clear .venv
+.\.venv\Scripts\python -m pip install -U pip setuptools
+goto syncenv
+
+:syncenv
+.\.venv\Scripts\python -m pip install -Ur .\tools\dev-requirements.txt
+exit /B %ERRORLEVEL%
+
+:help
+echo Usage:
+echo   make ^<command^>
+echo.
+echo Commands:
+echo   reformat                   Reformat all .py files being tracked by git.
+echo   stylecheck                 Check which tracked .py files need reformatting.
+echo   newenv                     Create or replace this project's virtual environment.
+echo   syncenv                    Sync this project's virtual environment to Red's latest
+echo                              dependencies.
